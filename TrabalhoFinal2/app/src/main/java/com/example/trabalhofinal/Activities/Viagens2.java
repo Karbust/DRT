7f4f0e20d8842ff824f6d9b32c8676020a84d31d
Notorious Adri <61848792+NotoriousAdri@users.noreply.github.com>
2020-06-28 21:07:41 +0100

    Revert "gdf"
    This reverts commit 6cc3322cfd8100a90ddcd1a0d8ad58ec6b9610c9.

--- TrabalhoFinal2/app/src/main/java/com/example/trabalhofinal/Activities/Viagens2.java
+++ TrabalhoFinal2/app/src/main/java/com/example/trabalhofinal/Activities/Viagens2.java
@@ -1,4 +1,4 @@
-package com.example.trabalhofinal;
+package com.example.trabalhofinal.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-<<<<<<< HEAD:TrabalhoFinal2/app/src/main/java/com/example/trabalhofinal/Activities/Viagens2.java
 import com.example.trabalhofinal.Api.RetrofitClient;
 import com.example.trabalhofinal.Models.Domain.Viagem;
 import com.example.trabalhofinal.Models.Responses.LoginResponse;
@@ -32,9 +31,6 @@ public class Viagens2 extends AppCompatActivity implements View.OnClickListener 
     private static final String TAG = "Viagens2:";
     private ApplicationContext applicationContext;
     private SharedPrefManager sharedPrefManager;
-=======
-public class Viagens2 extends AppCompatActivity {
->>>>>>> parent of 26a9067... 28/06 20:38:TrabalhoFinal2/app/src/main/java/com/example/trabalhofinal/Viagens2.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
